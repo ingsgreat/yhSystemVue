e353a03697c8f7611943300a91900bd202afced1
nanxia-xhy <2877053244@qq.com>
2021-08-12 16:41:02 +0800

    前端vue界面已完成

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="header">
+    <h1><p>桂林市公证处摇号系统</p></h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header{
+  width: 100%;
+  height: 100px;
+  background-color: #5880b0;
+}
+p{
+  font-size: 35px;
+  font-family:黑体;
+  color: #000000;
+  padding: 20px 200px;
+}
+
+</style>
